1097b98dd23385c65023aa0fd36132a83d4a5ca8
Hapsari Laksmi Wijayanti <hapsari.laksmiw@gmail.com>
2018-04-27 16:06:39 +0700

    add folder doctor and template

---
+++ pages/doktor/tempDok.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="doktor.css">
+    <script type="text/javascript" src="doktor.js"></script>
+</head>
+<body>
+
+<div id="pageBar">
+    <div class="sidenav">
+            <img id="fotoProfil" src="img/user.png" alt="fotoDokter">
+            <p>Nama Dokter</p></a>
+            <p>ID Dokter</p></a>
+            <p>JABATAN</p>
+            <p>Spesialis</p>
+            <p>Email</p>
+            <a href="#"><img id="logout" style="width: 20px; height: 20px" src='img/logout.png'/><h4 id="keluar">keluar</h4></a>
+    </div>
+
+    <div class="content">
+        <h2>Halaman Dokter</h2>
+        <div id="menubar">
+            <ul>
+                <li><a class="active" href="#home">Home</a></li>
+                <li><a href="#news">News</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#about">About</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+
+
+<div>
+
+</div>
+</body>
+</html>
