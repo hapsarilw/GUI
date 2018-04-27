40aa4be600f885d93551fc1cec3788a936db007c
Hapsari Laksmi Wijayanti <hapsari.laksmiw@gmail.com>
2018-04-27 17:11:01 +0700

    template done

--- pages/doktor/tempDok.php
+++ pages/doktor/tempDok.php
@@ -3,9 +3,15 @@
 <head>
     <link rel="stylesheet" type="text/css" href="doktor.css">
     <script type="text/javascript" src="doktor.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-
+<div class="topnav">
+    <a class="active" href="#home">Home</a>
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+</div>
 <div id="pageBar">
     <div class="sidenav">
             <img id="fotoProfil" src="img/user.png" alt="fotoDokter">
@@ -16,18 +22,10 @@
             <p>Email</p>
             <a href="#"><img id="logout" style="width: 20px; height: 20px" src='img/logout.png'/><h4 id="keluar">keluar</h4></a>
     </div>
+</div>
 
-    <div class="content">
-        <h2>Halaman Dokter</h2>
-        <div id="menubar">
-            <ul>
-                <li><a class="active" href="#home">Home</a></li>
-                <li><a href="#news">News</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li><a href="#about">About</a></li>
-            </ul>
-        </div>
-    </div>
+<div class="main">
+    <p>Cek</p>
 </div>
 
 
