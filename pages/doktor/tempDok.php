dcad43157b61ba7831eecfe363a368387077875f
Andi Tangguh <anditangguh71@yahoo.com>
2018-04-28 17:59:08 +0700

    update tempdok

--- pages/doktor/tempDok.php
+++ pages/doktor/tempDok.php
@@ -15,12 +15,13 @@
             <div class="sidenav">
                 <img id="fotoProfil" src="img/user.png" alt="fotoDokter">
                 <p id="namaD">Nama Dokter</p>
-                <p>ID Dokter</p></a>
-            <p>JABATAN</p>
-            <p>Spesialis</p>
-            <p>Email</p>
-            <div id="logout">
-                <a href="../karyawan/karyawanlogin.html"><img style="width: 20px; height: 20px" src='img/logout.png'/><h4 id="keluar">keluar</h4></a>
+                <p id="idD">ID Dokter</p>
+                <p id="jabatan">JABATAN</p>
+                <p id="jenis_spesialisasi">Spesialis</p>
+                <p id="emailD">Email</p>
+                <div id="logout">
+                    <a href="../karyawan/karyawanlogin.html"><img style="width: 20px; height: 20px" src='img/logout.png'/><h4 id="keluar">keluar</h4></a>
+                </div>
             </div>
         </div>
     </body>
